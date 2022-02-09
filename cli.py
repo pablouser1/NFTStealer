6e46a7753bae1fc2d6fc31a15546352550bf816f
Pablo Ferreiro <pferreiromero@gmail.com>
2022-02-09 21:49:10 +0100

    FIREFOX_BIN in Cli

--- cli.py
+++ cli.py
@@ -1,6 +1,8 @@
 from nftstealer.Stealer import Stealer
+from dotenv import load_dotenv
 
 if __name__ == '__main__':
+    load_dotenv()
     stealer = Stealer()
     print("Getting NFT...")
     link = stealer.randomLink()
